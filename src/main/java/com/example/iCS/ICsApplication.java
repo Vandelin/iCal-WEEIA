0d4b75128d579e77ef91efc86284623648bc85a4
Piotr Rogowski <polishotaku@gmail.com>
2019-11-19 16:35:57 +0100

    Added dependency and controller

---
+++ src/main/java/com/example/iCS/ICsApplication.java
@@ -0,0 +1,13 @@
+package com.example.iCS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ICsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iCS/ICsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iCS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ICsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
